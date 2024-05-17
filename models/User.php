63e82659ac56047a0131f5f3702949324a476d38
Vlad Usepusev <gerasimovv39@gmail.com>
2024-05-17 13:10:53 +0300

    first commit

---
+++ models/User.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string|null $login
+ * @property string|null $password
+ * @property string|null $email
+ * @property string|null $phone
+ * @property string|null $fio
+ * @property int|null $role_id
+ *
+ * @property Report[] $reports
+ * @property Role $role
+ */
+class User extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface                   
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['role_id'], 'integer'],
+            [['login', 'paassword', 'email', 'phone', 'fio'], 'required', 'message'=> 'Поле не заполнено'],
+            [['email'], 'email', 'message'=> 'Некорректно введена электронная почта' ],
+            [['phone'], 'string', 'min'=>11, 'max'=>11, 'tooShort'=>'Номер телефлна слишком короткий', 'tooLong'=>'Номер телефлна слишком длинный'],
+            [['login', 'password', 'email', 'phone', 'fio'], 'string', 'max' => 255],
+            [['role_id'], 'exist', 'skipOnError' => true, 'targetClass' => Role::class, 'targetAttribute' => ['role_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'login' => 'Логин',
+            'password' => 'Пароль',
+            'email' => 'Электронная почта',
+            'phone' => 'Номер телефона',
+            'fio' => 'ФИО',
+        ];
+    }
+
+    /**
+     * Gets query for [[Reports]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getReports()
+    {
+        return $this->hasMany(Report::class, ['user_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Role]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getRole()
+    {
+        return $this->hasOne(Role::class, ['id' => 'role_id']);
+    }
+
+    
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentity($id)
+    {
+        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return null;
+    }
+
+    /**
+     * Finds user by username
+     *
+     * @param string $username
+     * @return static|null
+     */
+    public static function findByUsername($username)
+    {
+            return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAuthKey()
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validateAuthKey($authKey)
+    {
+        return null;
+    }
+
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return bool if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return $this->password === $password;
+    }
+
+    public static function login($login, $password) {
+        $user=self::findOne(['login' => $login]);
+        if($user && $user->validatePassword ($password)) {
+            return $user;
+        }
+        return null;
+    } 
+}
---
+++ views/site/index.php
@@ -0,0 +1,93 @@
+<img?php
+
+/** @var yii\web\View $this */
+
+?>
+<div class="site-index">
+
+<main>
+
+  <div id="myCarousel" class="carousel slide mb-6" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" class=""></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" class="active" aria-current="true"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item">
+      <div class="MMImageContainer"><img class="MMImage-Preview" src="https://sportishka.com/uploads/posts/2023-12/1701537009_sportishka-com-p-mashini-gai-vkontakte-43.jpg" alt="Машины гаи - 76 фото" data-load-timing="1" role="presentation" style="max-height: 849px;" width="100%" height="100%" fill="var(--bs-secondary-color)"></div>      <div class="container">
+          <div class="carousel-caption text-start">
+            <p class="opacity-75">Мы отслеживаем, в каких районах чаще всего происходит правонарушения и передаем сведения в ГИБДД.</p>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+      <div class="MMImageContainer"><img class="MMImage-Preview" src="//avatars.mds.yandex.net/i?id=439951e8866f5cdf8abf08a396f5cc94_l-9099806-images-thumbs&amp;n=13" alt="Дпс это" data-load-timing="1" role="presentation" style="max-height: 849px;" width="100%" height="100%" fill="var(--bs-secondary-color)"></div>        <div class="container">
+          <div class="carousel-caption">
+            <p>Мы контролируем, чтобы каждое одобренное заявление было рассмотрено соответствующим органом.</p>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item active">
+      <div class="MMImageContainer"><img class="MMImage-Preview" src="//avatars.mds.yandex.net/i?id=29ea1511b1753ec7bd120a99c78bfbd9_l-5236521-images-thumbs&amp;n=13" alt="Новости - SB Group - юридическая компания" data-load-timing="1" role="presentation" style="max-height: 849px; "width="100%" height="100%" fill="var(--bs-secondary-color)"></div>
+        <div class="container">
+          <div class="carousel-caption text-end">
+            <p>Мы хотим сделать этот мир лучше с Вами!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+
+
+
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading fw-normal lh-1">Нарушением нет </h2>
+        <p class="lead">Портал “Нарушением нет” является передовой платформой по приему заявок о дорожных правонарушениях. </p>
+      </div>
+      <div class="col-md-5">
+      <div class="MMImageContainer"><img class="MMImage-Preview" src="//avatars.mds.yandex.net/i?id=c01aee61e0545954f9239017dae71acd_l-4954618-images-thumbs&amp;n=13" alt="Фото Дпсника - 61 фото" data-load-timing="1" role="presentation" style="max-height: 849px;" width="100%" height="100%"></div>
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7 order-md-2">
+        <h2 class="featurette-heading fw-normal lh-1">Сознательные граждане </h2>
+        <p class="lead">Сознательные граждане со всей России оставляют здесь свои заявления, когда не знают, куда обратиться. </p>
+      </div>
+      <div class="col-md-5 order-md-1">
+      <div class="MMImageContainer"><img class="MMImage-Preview" src="//avatars.mds.yandex.net/i?id=b3186f9051dec5f3a403ffdc7eb5d2ff_l-4012127-images-thumbs&amp;n=13" alt="Как ГИБДД будет штрафовать водителей за неисправные фары? - 30 Марта 2023 - Club" data-load-timing="1" role="presentation" style="max-height: 849px;" width="100%" height="100%"></div>      </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading fw-normal lh-1">Каждое обращение граждан</h2>
+        <p class="lead">Каждое обращение граждан доходит до государственной инспекции безопасности дорожного движения (ГИБДД) и проверяется на соответствия всем нормам и правил дорожного движения. </p>
+      </div>
+      <div class="col-md-5 order-md-1">
+      <div class="MMImageContainer"><img class="MMImage-Preview" src="//avatars.mds.yandex.net/i?id=dfa40d17d886e11d612897c9ee9cfaaf_l-5236988-images-thumbs&amp;n=13" alt="obshenie_s_gibdd техпомощь22.рф" data-load-timing="1" role="presentation" style="max-height: 849px;" width="100%" height="100%"></div>
+      </div>
+    </div>
+
+
+    <!-- /END THE FEATURETTES -->
+
+  </div><!-- /.container -->
+
+
+  <!-- FOOTER -->
+</main>
+</div>
